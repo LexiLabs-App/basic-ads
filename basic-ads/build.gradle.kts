5f0955790cf90a4441c7363e390d4ed5874fc8ae
Robert Jamison <65142411+robertjamison@users.noreply.github.com>
2026-07-11 18:55:47 -0400

    gradle=9.6.1

--- basic-ads/build.gradle.kts
+++ basic-ads/build.gradle.kts
@@ -11,45 +11,6 @@ plugins {
     alias(libs.plugins.kover)
 }
 
-/** REMEDIATION **/
-//dependencies {
-//    /** This patches all transitive dependencies that have vulnerabilities **/
-//    constraints {
-//        androidMainImplementation(libs.remediate.okhttp) {
-//            version { strictly(libs.versions.remediate.okhttp.get()) }
-//            because("CVE-2021-0341")
-//        }
-//        androidMainImplementation(libs.remediate.bitbucket) {
-//            version { strictly(libs.versions.remediate.bitbucket.get())}
-//            because("CVE-2024-29371")
-//        }
-//        androidMainImplementation(libs.remediate.netty.codec.http){
-//            version { strictly(libs.versions.remediate.netty.get())}
-//            because("CVE-2025-67735")
-//        }
-//        androidMainImplementation(libs.remediate.netty.codec.http2){
-//            version { strictly(libs.versions.remediate.netty.get())}
-//            because("CVE-2025-55163")
-//        }
-//        androidMainImplementation(libs.remediate.google.protobuf.kotlin){
-//            version { strictly(libs.versions.remediate.google.protobuf.get())}
-//            because("CVE-2024-7254")
-//        }
-//        androidMainImplementation(libs.remediate.google.protobuf.java){
-//            version { strictly(libs.versions.remediate.google.protobuf.get())}
-//            because("CVE-2024-7254")
-//        }
-//        androidMainImplementation(libs.remediate.jdom){
-//            version { strictly(libs.versions.remediate.jdom.get())}
-//            because("CVE-2021-33813")
-//        }
-//        androidMainImplementation(libs.remediate.apache.compress){
-//            version { strictly(libs.versions.remediate.apache.compress.get())}
-//            because("CVE-2024-26308")
-//        }
-//    }
-//}
-
 kotlin {
 
     // FORCES CHECK OF PUBLIC API DECLARATIONS
